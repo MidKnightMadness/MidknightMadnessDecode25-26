707406914e15a7ae1d3b9e3e64dca6921d70c801
seweed3 <matthewc1172@gmail.com>
2025-09-18 19:53:28 -0700

    feat(DoubleFlywheelTest): Replace internal velocity PIDF controller with custom PIDF power controller

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Outake/DoubleFlywheelTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Outake/DoubleFlywheelTest.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.Outake;
 
+import static java.lang.Math.clamp;
+
 import com.bylazar.configurables.annotations.Configurable;
 import com.bylazar.graph.GraphManager;
 import com.bylazar.telemetry.TelemetryManager;
@@ -31,27 +33,26 @@ public class DoubleFlywheelTest extends OpMode {
     TelemetryManager panelsTelemetry;
 
 
-    //from motor specs rev
+    //motor constants
     final double ticksPerRevolution = 28;
+    public static double wheelDiameter =  0.07;//in m
 
 
     Timer timer;
-    //public static double power = 1;
-    //Replaced power with rpm
 
     //change left and right rpm to add spin, can edit through FTC panels under configurables
-    public static double leftRpm = 1300;
-    public static double rightRpm = 1000;
+    public static double targetLeftRpm = 1600;
+    public static double targetRightRpm = 1300;
 
 
-    //diameter of the wheel to calculate approximate exit velocity(not needed right now)
-    public static double wheelDiameter =  0.07;//in m
 
+    public static double maxRPM = 2100;
     //PIDF gains
     public static double pGain = 0.03;//Really only need p and maybe f gains
     public static double iGain = 0;
     public static double dGain = 0;
-    public static double fGain = 0;
+    public static double fGain = 1/ maxRPM;
+
 
     @Override
     public void init() {
@@ -69,8 +70,6 @@ public class DoubleFlywheelTest extends OpMode {
         panelsTelemetry = PanelsTelemetry.INSTANCE.getTelemetry();
         timer = new Timer();
 
-//        left.setVelocityPIDFCoefficients(pGain, iGain, dGain, fGain);
-//        right.setVelocityPIDFCoefficients(pGain, iGain, dGain, fGain);
     }
 
 
@@ -81,6 +80,10 @@ public class DoubleFlywheelTest extends OpMode {
         left.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         right.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
+    double leftIntegralComponent = 0;
+    double rightIntegralComponent = 0;
+    double prevLeftError = 0;
+    double prevRightError = 0;
 
     @Override
     public void loop() {
@@ -90,55 +93,84 @@ public class DoubleFlywheelTest extends OpMode {
             resetEncoders();
         }
 
+        //Assuming we want different speeds for the flywheel
+        //Get current rpms
+        double currLeftRpm = left.getVelocity() / ticksPerRevolution * 60 / fourToOneGR;
+        double currRightRpm = right.getVelocity() / ticksPerRevolution * 60 / fourToOneGR;
 
-        //Set the motors to the target velocities from the target RPMs
-        double targetLeftRPMNom = leftRpm * fourToOneGR;
-        double targetLeftVel = targetLeftRPMNom / 60 * ticksPerRevolution;//ticks per revolution
+        //PIDF logic
+        double leftError = targetLeftRpm - currLeftRpm;
+        double rightError = targetRightRpm - currRightRpm;
 
-        double targetRightRPMNom = rightRpm * fourToOneGR;
-        double targetRightVel = targetRightRPMNom / 60 * ticksPerRevolution;//ticks per revolution
+        double deltaTime = timer.getLastUpdateTime();
+        leftIntegralComponent += leftError * deltaTime;
+        rightIntegralComponent += rightError * deltaTime;
 
-        telemetry.addData("Target Left", leftRpm);
-        telemetry.addData("Target Right", rightRpm);
-        telemetry.addData("Target Left Nom", targetLeftRPMNom);
-        telemetry.addData("Target Right Nom", targetRightRPMNom);
+        double leftDerivative = (leftError - prevLeftError) / deltaTime;
+        double rightDerivative = (rightError - prevRightError) / deltaTime;
 
-        left.setVelocity(targetLeftVel);
-        right.setVelocity(targetRightVel);
+        double leftOutput = pGain * leftError + iGain * leftIntegralComponent + dGain * leftDerivative + fGain * targetLeftRpm;
+        double rightOutput = pGain * rightError + iGain * rightIntegralComponent + dGain * rightDerivative + fGain * targetRightRpm;
 
-        telemetry.addData("Left Vel:", targetLeftVel);
-        telemetry.addData("Right Vel:", targetRightVel);
+        double leftPower = (leftOutput <= 1 &&  leftOutput>=-1) ? leftOutput : leftOutput > 1 ? 1 : -1;
+        double rightPower = (rightOutput <= 1 &&  rightOutput>=-1) ? rightOutput : rightOutput > 1 ? 1 : -1;
 
-//        left.setPower(power);
-//        right.setPower(power);
+        left.setPower(leftPower);
+        right.setPower(rightPower);
 
-        double leftRpmNominal = left.getVelocity() / ticksPerRevolution * 60;
-        double rightRpmNominal = right.getVelocity() / ticksPerRevolution * 60;
-        double leftRpmActual = leftRpmNominal / fourToOneGR;
-        double rightRpmActual = rightRpmNominal / fourToOneGR;
+        telemetry.addData("Update Rate", 1/ deltaTime);
+        telemetry.addData("Target Left", targetLeftRpm);
+        telemetry.addData("Target Right", targetRightRpm);
+
+        telemetry.addData("Left Vel:", currLeftRpm);
+        telemetry.addData("Right Vel:", currRightRpm);
 
         double wheelCircumference = Math.PI * wheelDiameter;
-        double leftExitVel = leftRpmActual / 60 * wheelCircumference;
-        double rightExitVel = rightRpmActual / 60 * wheelCircumference;
+        double leftExitVel = currLeftRpm / 60 * wheelCircumference;
+        double rightExitVel = currRightRpm / 60 * wheelCircumference;
 
 
         //Display data on Ftc Panels: navigate to 192.168.43.1:8001 on robot wifi
-        graph.addData("Left RPM Actual", leftRpmActual);
-        graph.addData("Right RPM Actual", rightRpmActual);
-        graph.addData("Left RPM Nominal", leftRpmNominal);
-        graph.addData("Right RPM Nominal", rightRpmNominal);
+        graph.addData("Left Current RPM", currLeftRpm);
+        graph.addData("Right Current RPM", currRightRpm);
+
+
+        //PID values
+        graph.addData("Left Error", leftError);
+        graph.addData("Right Error", rightError);
+        graph.addData("Left Integral", leftIntegralComponent);
+        graph.addData("Right Integral", rightIntegralComponent);
+        graph.addData("Left Derivative", leftDerivative);
+        graph.addData("Right Derivative", rightDerivative);
+        graph.addData("Left Output", leftOutput);
+        graph.addData("Right Output", rightOutput);
+
+        graph.addData("Update rate", 1/ deltaTime);
         graph.addData("Average Exit Velocity", (leftExitVel + rightExitVel) / 2);
-//        graph.addData("Power", power);
         graph.update();
 
-        panelsTelemetry.addData("Left rpm nominal", leftRpmNominal);
-        panelsTelemetry.addData("Right rpm nominal", rightRpmNominal);
-        panelsTelemetry.addData("Left rpm actual", leftRpmActual);
-        panelsTelemetry.addData("Right rpm actual", rightRpmActual);
-        panelsTelemetry.addData("Update rate", 1/ timer.getLastUpdateTime());
+
+        panelsTelemetry.addData("Left Target RPM", targetLeftRpm);
+        panelsTelemetry.addData("Left Current RPM", currLeftRpm);
+        panelsTelemetry.addData("Right Target RPM", targetRightRpm);
+        panelsTelemetry.addData("Right Current RPM", currRightRpm);
+
+        //PID values
+        panelsTelemetry.addData("Left Error", leftError);
+        panelsTelemetry.addData("Right Error", rightError);
+        panelsTelemetry.addData("Left Integral", leftIntegralComponent);
+        panelsTelemetry.addData("Right Integral", rightIntegralComponent);
+        panelsTelemetry.addData("Left Derivative", leftDerivative);
+        panelsTelemetry.addData("Right Derivative", rightDerivative);
+        panelsTelemetry.addData("Left Output", leftOutput);
+        panelsTelemetry.addData("Right Output", rightOutput);
+
+        panelsTelemetry.addData("Update rate", 1/ deltaTime);
         panelsTelemetry.addData("Average Exit Velocity", (leftExitVel + rightExitVel) / 2);
-//        panelsTelemetry.addData("Power", power);
 
         panelsTelemetry.update(telemetry);
+
+        prevLeftError = leftError;
+        prevRightError = rightError;
     }
 }
