77a1f13eb38883c5ebccebd3afb93d1b382167d7
seweed3 <matthewc1172@gmail.com>
2025-09-26 20:38:19 -0700

    feat(Vision): Read/write to Android File Transfer for Auto->TeleOp Periods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTags/MotifAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTags/MotifAutonomous.java
@@ -1,82 +1,103 @@
 package org.firstinspires.ftc.teamcode.AprilTags;
 
-import com.bylazar.configurables.annotations.Configurable;
 import com.bylazar.telemetry.PanelsTelemetry;
 import com.bylazar.telemetry.TelemetryManager;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import android.os.Environment;
 
 import org.firstinspires.ftc.teamcode.Util.Timer;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
-@TeleOp(group = "Vision", name = "AprilTagTesting")
-@Configurable
-public class AprilTagTesting extends OpMode{
-
+@Autonomous(group = "Vision", name = "MotifAutonomous")
+public class MotifAutonomous extends OpMode{
     private Limelight3A limelight;
     public static String configName = "limelight";
-    private int leftPipeline = 0;
     private int motifPipeline = 1;
-    private int rightPipeline = 2;
-
-    public static int currentPipeline = 1;
-    private int previousPipelineNum = 1;
     TelemetryManager panelsTelemetry;
     Timer timer;
+    boolean finishedWriting = false;
+    double detectionMaxTime = 3;
 
-
+    PrintWriter pw;
+    String fileName = "motif_value.txt";
+    String directoryName = "Vision";
+    File file;
+    FileWriter fileWriter;
     @Override
-    public void init() {
+    public void init(){
 
-        timer = new Timer();
-        limelight = hardwareMap.get(Limelight3A.class, configName);
-        telemetry.setMsTransmissionInterval(5);
-        limelight.pipelineSwitch(currentPipeline);//set it to motif pipeline
-        limelight.start();
         panelsTelemetry = PanelsTelemetry.INSTANCE.getTelemetry();
+        timer = new Timer();
+
+        limelight = hardwareMap.get(Limelight3A.class, configName);
+        limelight.pipelineSwitch(motifPipeline);
+        limelight.start();
+
+        file = createFile(fileName, directoryName);
+        try {
+            fileWriter = new FileWriter(file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
+    private static File createFile(String fileName, String dirName){
+        File dir = new File(Environment.getExternalStorageDirectory(), dirName);
+        if(!dir.exists()){
+            dir.mkdirs();
+        }
 
+        File file = new File(dir, fileName);
+        return file;
+    }
 
 
     @Override
-    public void loop() {
+    public void init_loop(){
 
-        timer.updateTime();
-        telemetry.addData("Update rate", 1/ timer.getDeltaTime());
-
-
-        if(previousPipelineNum != currentPipeline){
-            limelight.pipelineSwitch(currentPipeline);
-            telemetry.addLine("Swapping to pipeline " + currentPipeline);
-        }
-
-
+        if(finishedWriting == false && timer.updateTime() < detectionMaxTime) {
             LLResult result = limelight.getLatestResult();
             if (result != null && result.isValid()) {
                 List<LLResultTypes.FiducialResult> list = result.getFiducialResults();
                 for (LLResultTypes.FiducialResult f : list) {
                     updatePanelsTelemetry(f);
-                    telemetry.addLine("April Tag ID" +  f.getFiducialId());
-                    telemetry.addLine("Corners" +  f.getTargetCorners().toString());
-                    telemetry.addLine("Camera Pos" +  f.getCameraPoseTargetSpace().toString());
+                    telemetry.addLine("April Tag ID" + f.getFiducialId());
+                    telemetry.addLine("Corners" + f.getTargetCorners().toString());
+                    telemetry.addLine("Camera Pos" + f.getCameraPoseTargetSpace().toString());
                     telemetry.addLine("Robot Pos Field" + f.getRobotPoseFieldSpace().toString());
                     telemetry.addLine("Robot Pos Target" + f.getRobotPoseTargetSpace().toString());
                     telemetry.addLine("X Angle" + f.getTargetXDegrees());
                     telemetry.addLine("Y Angle" + f.getTargetYDegrees());
+                    try {
+                        fileWriter.write(f.getFiducialId());
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
+                finishedWriting = true;
             }
+        }
 
-        previousPipelineNum = currentPipeline;
+        if(timer.updateTime() > detectionMaxTime) {
+            try {
+                fileWriter.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
-
     public void updatePanelsTelemetry(LLResultTypes.FiducialResult f){
         panelsTelemetry.addLine("April Tag ID" +  f.getFiducialId());
         panelsTelemetry.addLine("Corners" +  f.getTargetCorners().toString());
@@ -85,6 +106,11 @@ public class AprilTagTesting extends OpMode{
         panelsTelemetry.addLine("Robot Pos Target" + f.getRobotPoseTargetSpace().toString());
         panelsTelemetry.addLine("X Angle" + f.getTargetXDegrees());
         panelsTelemetry.addLine("Y Angle" + f.getTargetYDegrees());
+        panelsTelemetry.update();
     }
 
+    @Override
+    public void loop() {
+
+    }
 }
