b921d265de6ff95d682e2f9c37a2cfd200ea19da
seweed3 <matthewc1172@gmail.com>
2025-10-03 16:02:34 -0700

    feat(Localization): Fix April Tag Localization

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Localization/LocalizationTest.java
@@ -0,0 +1,171 @@
+package org.firstinspires.ftc.teamcode.Localization;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.bylazar.configurables.annotations.Configurable;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.teamcode.Util.ButtonToggle;
+import org.firstinspires.ftc.teamcode.Util.PoseBuffer;
+import org.firstinspires.ftc.teamcode.Util.Timer;
+
+import java.util.List;
+
+@TeleOp(group = "Localization", name = "LocalizationTest")
+@Configurable
+public class LocalizationTest extends OpMode{
+
+    private Limelight3A limelight;
+    public static String configName = "limelight";
+    private static int leftPipeline = 0;
+    private static int motifPipeline = 1;
+    private static int rightPipeline = 2;
+
+    public static int currentPipeline = leftPipeline;
+    private int previousPipelineNum = leftPipeline;
+    TelemetryManager panelsTelemetry;
+    Timer timer;
+    PoseBuffer mt1Buffer;
+    PoseBuffer mt2Buffer;
+    ButtonToggle x1;
+    RevHubOrientationOnRobot.LogoFacingDirection logoFacingDirection;
+    RevHubOrientationOnRobot.UsbFacingDirection usbFacingDirection;
+    Pose3D megaTag1Pose;
+    Pose3D megaTag2Pose;
+
+    IMU imu;
+
+    @Override
+    public void init() {
+
+        timer = new Timer();
+        limelight = hardwareMap.get(Limelight3A.class, configName);
+        limelight.pipelineSwitch(currentPipeline);//set it to motif pipeline
+        telemetry.setMsTransmissionInterval(10);
+        limelight.start();
+        panelsTelemetry = PanelsTelemetry.INSTANCE.getTelemetry();
+
+        x1 = new ButtonToggle();
+        mt1Buffer = new PoseBuffer();
+        mt2Buffer = new PoseBuffer();
+
+        logoFacingDirection = RevHubOrientationOnRobot.LogoFacingDirection.UP;
+        usbFacingDirection = RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
+        imu = hardwareMap.get(IMU.class, "imu");
+        imu.initialize(new IMU.Parameters(new RevHubOrientationOnRobot(logoFacingDirection, usbFacingDirection)));
+    }
+
+
+
+    private void createDashboardTelemetry(){
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
+    }
+
+
+    public Pose3D convertMetersToInch(Pose3D pose){
+        Position position = pose.getPosition();
+        double x = position.x * 100 / 2.54;
+        double y = position.y * 100 / 2.54;
+        double z = position.z * 100 / 2.54;
+        return new Pose3D(new Position(DistanceUnit.INCH, x, y, z, 0), pose.getOrientation());
+
+    }
+    @Override
+    public void loop() {
+        YawPitchRollAngles imuAngles = imu.getRobotYawPitchRollAngles();
+        limelight.updateRobotOrientation(imuAngles.getYaw(AngleUnit.DEGREES));
+
+
+        createDashboardTelemetry();
+
+        timer.updateTime();
+
+        telemetry.addData("Update rate", 1/ timer.getDeltaTime());
+        telemetry.addData("Yaw(Deg): ", imuAngles.getYaw(AngleUnit.DEGREES));
+
+        if(previousPipelineNum != currentPipeline){
+            limelight.pipelineSwitch(currentPipeline);
+            telemetry.addLine("Swapping to pipeline " + currentPipeline);
+        }
+
+        if(x1.update(gamepad1.x)) {
+
+            LLResult result = limelight.getLatestResult();
+
+            if (result != null && result.isValid()) {
+
+                megaTag1Pose = result.getBotpose();
+                megaTag1Pose = convertMetersToInch(megaTag1Pose);
+
+                megaTag2Pose = result.getBotpose_MT2();
+                megaTag2Pose = convertMetersToInch(megaTag2Pose);
+
+
+                telemetry.addData("MT1Pose: ", megaTag1Pose.toString());
+                telemetry.addData("MT1Position(Buffered): ", megaTag1Pose.toString());
+                telemetry.addData("MT2Pose: ", megaTag2Pose.toString());
+                telemetry.addData("MT2Position(Buffered): ", megaTag1Pose.toString());
+
+                List<LLResultTypes.FiducialResult> list = result.getFiducialResults();
+
+                telemetry.addLine("--------------");
+                for (LLResultTypes.FiducialResult f : list) {
+                    Position camPoseTarget = f.getCameraPoseTargetSpace().getPosition().toUnit(DistanceUnit.INCH);
+                    updatePanelsTelemetry(f, camPoseTarget, megaTag1Pose.getPosition());
+
+                    telemetry.addData("April Tag ID", f.getFiducialId());
+//                    telemetry.addData("Corners",  f.getTargetCorners().toString());
+                    telemetry.addData("Camera Pos X",  camPoseTarget.x);
+                    telemetry.addData("Camera Pos Y",  camPoseTarget.y);
+                    telemetry.addData("Camera Pos Z",  camPoseTarget.z);
+                    telemetry.addData("Heading(Deg) Field",  f.getCameraPoseTargetSpace().getOrientation().getYaw(AngleUnit.DEGREES));
+                    telemetry.addData("X Angle",  f.getTargetXDegrees());
+                    telemetry.addData("Y Angle", f.getTargetYDegrees());
+
+                }
+
+            }
+
+            previousPipelineNum = currentPipeline;
+            telemetry.update();
+
+        }
+
+    }
+
+
+    private void updatePanelsTelemetry(LLResultTypes.FiducialResult f, Position camPoseTarget, Position megaTag1){
+        panelsTelemetry.addData("April Tag ID",  f.getFiducialId());
+        panelsTelemetry.addData("Corners",  f.getTargetCorners().toString());
+        panelsTelemetry.addData("Camera Pos X",  camPoseTarget.x);
+        panelsTelemetry.addData("Camera Pos Y",  camPoseTarget.y);
+        panelsTelemetry.addData("Camera Pos Z",  camPoseTarget.z);
+        panelsTelemetry.addData("X Angle", f.getTargetXDegrees());
+        panelsTelemetry.addData("Y Angle",  f.getTargetYDegrees());
+
+        panelsTelemetry.addData("MegaTagLocalizationPose X", megaTag1.x);
+        panelsTelemetry.addData("MegaTagLocalizationPose Y", megaTag1.y);
+        panelsTelemetry.addData("MegaTagLocalizationPose Z", megaTag1.z);
+//
+//        panelsTelemetry.addData("Averaged MegaPose X", averagedPose.x);
+//        panelsTelemetry.addData("Averaged MegaPose Y", averagedPose.y);
+//        panelsTelemetry.addData("Averaged MegaPose Z", averagedPose.z);
+
+        panelsTelemetry.update();
+    }
+
+}
