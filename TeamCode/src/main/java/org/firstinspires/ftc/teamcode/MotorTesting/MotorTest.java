a22523bd1b7b1397f5989c42abcfea57b2606ccc
Allen Dai <allendai000@gmail.com>
2025-10-15 15:31:20 -0700

    what is this

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTesting/MotorTest.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.MotorTesting;
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@Configurable
+@TeleOp(group = "MotorTest")
+public class MotorTest extends OpMode {
+    public static String motorName = "motor";
+    public static double motorPower = 1;
+    private DcMotorEx motor;
+
+    @Override
+    public void init() {
+        motor = hardwareMap.get(DcMotorEx.class, motorName);
+    }
+
+    @Override
+    public void loop() {
+        motor.setPower(motorPower);
+    }
+}
