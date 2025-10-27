5892ef0d82434e823bb744da307f28586004d557
seweed3 <matthewc1172@gmail.com>
2025-10-27 13:09:35 -0700

    add(double flywheel): second LUT account for spin

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/ConfigNames.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/ConfigNames.java
@@ -9,6 +9,15 @@ public class ConfigNames {
     public static String imu = "imu";
     public static String pinpoint = "pinpoint";
     public static String limelight = "limelight";
-    public static String lowFlywheel = "lowFlywheel";
-    public static String highFlywheel = "highFlywheel";
+    public static String lowFlywheelMotor = "lowFlywheelMotor";
+    public static String highFlywheelMotor = "highFlywheelMotor";
+    public static String FL = "FL";
+    public static String FR = "FR";
+    public static String BL = "BL";
+    public static String BR = "BR";
+    public static String spinServo = "spindexterServo";
+    public static String rampServo = "rampServo";
+    public static String intakeColor = "intakeColor";
+    public static String outakeColor = "outakeColor";
+
 }
