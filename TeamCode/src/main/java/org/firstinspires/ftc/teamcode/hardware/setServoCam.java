25f2c67182a0fa4f5542a8de1bf60a349df31996
Allen Dai <allendai000@gmail.com>
2026-01-10 16:11:43 -0800

    sss

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/setServoCam.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class setServoCam {
+    Servo servo;
+    private final double angle = 0;
+
+    static
+}
