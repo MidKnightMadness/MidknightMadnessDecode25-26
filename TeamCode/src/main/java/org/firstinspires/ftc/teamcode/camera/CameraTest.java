c20e6008c325cb2fbd23e3cbf3beb90392afb85d
Allen Dai <allendai000@gmail.com>
2026-01-12 15:30:19 -0800

    camera test class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/camera/CameraTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/camera/CameraTest.java
@@ -1,5 +1,49 @@
+package org.firstinspires.ftc.teamcode.camera;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.LLStatus;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+import java.util.List;
+
+public class CameraTest extends OpMode {
+    Limelight3A limelight;
+    LLResult result;
+    List<LLResultTypes.DetectorResult> detections;
+
+    @Override
+    public void init() {
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.setPollRateHz(100);
+        limelight.start();
+        limelight.pipelineSwitch(0);
+
+    }
+
+    @Override
+    public void loop() {
+        result = limelight.getLatestResult();
+        if (result != null && result.isValid()) {
+            double tx = result.getTx(); // How far left or right the target is (degrees)
+            double ty = result.getTy(); // How far up or down the target is (degrees)
+            double ta = result.getTa(); // How big the target looks (0%-100% of the image)
+
+            telemetry.addData("Target X", tx);
+            telemetry.addData("Target Y", ty);
+            telemetry.addData("Target Area", ta);
+        } else {
+            telemetry.addData("Limelight", "No Targets");
+        }
+        detections = result.getDetectorResults();
+        for (LLResultTypes.DetectorResult detection : detections) {
+            String className = detection.getClassName(); // What was detected
+            double x = detection.getTargetXDegrees(); // Where it is (left-right)
+            double y = detection.getTargetYDegrees(); // Where it is (up-down)
+            telemetry.addData(className, "at (" + x + ", " + y + ") degrees");
+        }
+    }
+}
