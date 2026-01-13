c012a171aff54bac2eba85ac0c5c75da9259d08f
Allen Dai <allendai000@gmail.com>
2026-01-13 09:31:15 -0800

    servo for camera

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/setServoCam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/setServoCam.java
@@ -4,8 +4,8 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class setServoCam {
-    Servo servo;
-    private final double angle = 0;
-
-    static
+    static void setCam(HardwareMap hardwareMap, double angle){
+        Servo servo = hardwareMap.get(Servo.class, "limelightServo");
+        servo.setPosition(angle);//needs testing too see what angle works
+    }
 }
