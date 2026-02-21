ee0d552e369db402ada0c05371bc9ef7bc92c050
merr <henry.bridgen@students.tka.org>
2026-02-20 17:39:35 -0800

    Revert "nitter"
    This reverts commit 049ee740e57a94cf5cb9ac10cc76ad4a9462e702.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/camera/AprilTagCameraTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/camera/AprilTagCameraTesting.java
@@ -45,8 +45,8 @@ public class AprilTagCameraTesting extends OpMode {
 
     private boolean lastLeft = false;
     private boolean lastRight = false;
-    public static double generalOffsetX = 0;
-    public static double generalOffsetY = 0;
+    public double generalOffsetX = 0;
+    public double generalOffsetY = 0;
 
     @Override
     public void init() {
