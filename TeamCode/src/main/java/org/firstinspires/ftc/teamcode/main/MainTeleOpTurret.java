dfab0bbe86c4d6ec070ff83a3769c6c30c1b3aae
Kevin <kevinzhang882019@gmail.com>
2026-04-23 13:15:14 -0700

    Merge

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/MainTeleOpTurret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/MainTeleOpTurret.java
@@ -115,7 +115,7 @@ public class MainTeleOpTurret extends CommandOpMode {
 
     SpindexerGotoPositionSmooth spindexerGotoPositionSmooth;
 
-//    Telemetry dashboardTelemetry;
+    //    Telemetry dashboardTelemetry;
     TwoWheelShooter2.ShootDist currentShootDist;
 
     public static boolean useDistanceSensor = true;
@@ -152,9 +152,7 @@ public class MainTeleOpTurret extends CommandOpMode {
     int activeSpindexerSpot = 0;
     public static double settleTime = 100;
     public static long fastSmoothTime = 500;
-    public static long fastInBetweenTime = 300;
-    public static long fastSmoothTime = 100;
-    public static int timerPerShot = 100;
+    public static long fastInBetweenTime = 120;
     public static double slowSmoothTime = 0.7;
     boolean driveFieldOriented = true;
     Limelight3A limelight;
@@ -327,9 +325,9 @@ public class MainTeleOpTurret extends CommandOpMode {
         if (spindexerBallCt != previousSpindexerBallCt) {
             controlLight1.setColor(
                     spindexerBallCt == 0 ? GobildaLightBlock.Color.RED :
-                    spindexerBallCt == 1 ? GobildaLightBlock.Color.ORANGE :
-                    spindexerBallCt == 2 ? GobildaLightBlock.Color.BLUE :
-                    GobildaLightBlock.Color.GREEN
+                            spindexerBallCt == 1 ? GobildaLightBlock.Color.ORANGE :
+                                    spindexerBallCt == 2 ? GobildaLightBlock.Color.BLUE :
+                                            GobildaLightBlock.Color.GREEN
             );
         }
         previousSpindexerBallCt = spindexerBallCt;
@@ -339,7 +337,7 @@ public class MainTeleOpTurret extends CommandOpMode {
         if(readyToShoot != previousReadyToShoot){
             expansionLight1.setColor(
                     !readyToShoot ? GobildaLightBlock.Color.YELLOW :
-                    GobildaLightBlock.Color.GREEN
+                            GobildaLightBlock.Color.GREEN
             );
         }
         previousReadyToShoot = readyToShoot;
@@ -349,7 +347,7 @@ public class MainTeleOpTurret extends CommandOpMode {
         if(arducamUse != previousArducamUse){
             controlLight2.setColor(
                     !arducamUse ? GobildaLightBlock.Color.YELLOW :
-                    GobildaLightBlock.Color.GREEN
+                            GobildaLightBlock.Color.GREEN
             );
         }
         previousArducamUse = arducamUse;
@@ -369,7 +367,7 @@ public class MainTeleOpTurret extends CommandOpMode {
         if(doneShooting != previousDoneShooting){
             expansionLight2.setColor(
                     !doneShooting ? GobildaLightBlock.Color.YELLOW :
-                    GobildaLightBlock.Color.GREEN
+                            GobildaLightBlock.Color.GREEN
             );
         }
         doneShooting = previousDoneShooting;
@@ -509,12 +507,9 @@ public class MainTeleOpTurret extends CommandOpMode {
         }
     }
     double diffRadians;
-    public static double driveMovingThreshold = 0.5;//in/sec
-    public static double driveRotatingThreshold = Math.toRadians(1);
-    public static double turretVelocityTheshold = Math.toRadians(0.5); //rad/s
-    double cameraLastReadTime = 0;
+    public static double velocityMovingThreshold = 1;//in/sec
+    public static double turretVelocityTheshold = Math.toRadians(3);//rad/s
     boolean driveStationary = true;
-    boolean driveRotateStationary = true;
     boolean turretStationary = true;
     double stableVisionHeading = 0;//RAD
     private void autoAlign() {
@@ -528,15 +523,6 @@ public class MainTeleOpTurret extends CommandOpMode {
         AprilTagDetection detection = null;
 
         if(gamepad1.bWasPressed()) {
-        driveStationary = Math.hypot(follower.getVelocity().getXComponent(), follower.getVelocity().getYComponent()) < driveMovingThreshold;
-        driveRotateStationary = Math.abs(follower.getVelocity().getTheta()) < driveRotatingThreshold;
-        turretStationary = Math.abs(turret.getEncoder().getRawVelocity()) < turretVelocityTheshold;;
-
-        //only read arducam bearing if stationary, use same correct turret heading as long as drivebase stationary
-//        if (driveStationary && turretStationary && arducamUse && stableVisionHeading == 0) {
-        boolean updateCamera = driveStationary && driveRotateStationary && turretStationary && arducamUse && timer.getTime() - cameraLastReadTime > 500;
-        if (gamepad1.b){
-            cameraLastReadTime = timer.getTime();
             arducam.update();
             for (AprilTagDetection tag : arducam.getDetectedTags()) {
                 detected = (shootSide == ShootSide.LEFT) ? (tag.id == 20) : (tag.id == 24);
@@ -552,7 +538,7 @@ public class MainTeleOpTurret extends CommandOpMode {
             aprilTagBearing = -Math.toRadians(detection.ftcPose.elevation);
         }
 
-        if (sotmEnabled) {
+        if(sotmEnabled) {
             double[] aimData;
 
             aimData = shooter.aimCalculator.targetPowersHeading(
@@ -561,14 +547,19 @@ public class MainTeleOpTurret extends CommandOpMode {
                     TwoWheelShooter2.getShootPoseNew(currentPose, shootSide)
             );
             targetHeading = MathFunctions.normalizeAngle(aimData[2]) + aprilTagBearing;
+            diffRadians = targetHeading - currentPose.getHeading();
+            wrappedTurretValue = Angle.fromRadians(diffRadians);
+            turret.setFieldAngleToServo(wrappedTurretValue);
         }
         else{//default back to face point algorithm
             targetHeading = TwoWheelShooter2.getShootHeading(currentPose, shootSide) + aprilTagBearing;
+            diffRadians = targetHeading - currentPose.getHeading();
+            wrappedTurretValue = Angle.fromRadians(diffRadians);
+            turret.setFieldAngleToServo(wrappedTurretValue);
         }
 
-        diffRadians = targetHeading - currentPose.getHeading();
-        wrappedTurretValue = Angle.fromRadians(diffRadians);
-        turret.setFieldAngleToServo(wrappedTurretValue);
+
+
 
         //wants wrapped turret angle between -2PI & 2 PI
         //270 current robot, want it to face 0:  0 - 270 + 360 = 90+ = ccw
@@ -603,7 +594,7 @@ public class MainTeleOpTurret extends CommandOpMode {
 
 
         if (!autoDriveToShoot && !driveFieldOriented) {
-            wheelControl.driveRelative(gamepad1.left_stick_y, -gamepad1.left_stick_x, gamepad1.right_stick_x, currSpeed);
+            wheelControl.driveRelative(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x, currSpeed);
         }
         else if(!autoDriveToShoot && driveFieldOriented){
             wheelControl.driveFieldCentric(
@@ -702,9 +693,6 @@ public class MainTeleOpTurret extends CommandOpMode {
             prepareSpindexer();
             outakeSpotsRotation = new OutakeSpotsRotation(spindexer, activeSpindexerSpot, fastInBetweenTime);
             activeSpindexerSpot = Math.max(activeSpindexerSpot - SpindexerNonCR.NUM_SPOTS, 0);
-//            spindexerGotoPositionSmooth = new SpindexerGotoPositionSmooth(spindexer, SpindexerSpotNonCR.getPositionFromIndex(activeSpindexerSpot, SpotType.INTAKE) + SpindexerSpotNonCR.OUTAKE_OFFSET_DEGREES / SpindexerNonCR.totalDegrees, fastSmoothTime);
-//            schedulePosition(spindexerGotoPositionSmooth);
-            outakeSpotsRotation = new OutakeSpotsRotation(spindexer, activeSpindexerSpot +3, timerPerShot);
             schedulePosition(outakeSpotsRotation);
         }
         else if(gamepad2.yWasPressed()){//SLOWEST: FOR SORTING - SMOOTHED
@@ -906,12 +894,8 @@ public class MainTeleOpTurret extends CommandOpMode {
         telemetry.addData("Target Heading Goal Align", Math.toDegrees(targetHeading));
         telemetry.addData("Turret Stationary", turretStationary);
         telemetry.addData("Robot Stationary", driveStationary);
-        telemetry.addData("Robot Rotate Stationary", driveRotateStationary);
-        telemetry.addData("Robot translational speed (in/sec)", Math.hypot(follower.getVelocity().getXComponent(), follower.getVelocity().getYComponent()));//in/sec
-        telemetry.addData("Robot angular speed (rad/sec)", follower.getVelocity().getTheta());
-        telemetry.addData("Robot angular speed 2 (rad/sec)", Math.abs(follower.getAngularVelocity()));
-
-
+        telemetry.addData("Vision Value", Math.toDegrees(stableVisionHeading));
+        telemetry.addData("Robot velocity Magnitude(in/sec)", follower.getVelocity().getMagnitude());//in/sec
         telemetry.addData("Turret velocity(deg/sec)", Math.toRadians(turret.getEncoder().getRawVelocity()));//deg/sec
 
 
@@ -969,9 +953,6 @@ public class MainTeleOpTurret extends CommandOpMode {
 //        dashboardTelemetry.addData("Transfer Error", (shooter.transferError));
         telemetry.addData("Active spindexer spot", activeSpindexerSpot);
         telemetry.addData("Spindexer Direct Position", spindexerDirectPosition);
-        if(outakeSpotsRotation != null) {
-            telemetry.addData("Shoot Timer timer", outakeSpotsRotation.getLastShotTime());
-        }
 
         telemetry.update();
 //        dashboardTelemetry.update();
